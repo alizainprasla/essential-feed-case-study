705bee9951fb51904853ebf5ac03769b16d60c02
alizainprasla <alizain@spiretech.co>
2022-03-07 21:20:06 +0500

    Revert "Merge pull request #64 from essentialdevelopercom/xcode13_0"
    This reverts commit 02beb90271bb07a4fc6665d2a8003096d96819a6, reversing
    changes made to 18ccf6188d9687e8221aaea8d77a653e3267460b.

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -9,18 +9,19 @@ import EssentialFeediOS
 class SceneDelegateTests: XCTestCase {
 	
 	func test_configureWindow_setsWindowAsKeyAndVisible() {
-		let window = UIWindowSpy()
+		let window = UIWindow()
 		let sut = SceneDelegate()
 		sut.window = window
 		
 		sut.configureWindow()
 		
-		XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+		XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
+		XCTAssertFalse(window.isHidden, "Expected window to be visible")
 	}
 	
 	func test_configureWindow_configuresRootViewController() {
 		let sut = SceneDelegate()
-		sut.window = UIWindowSpy()
+		sut.window = UIWindow()
 		
 		sut.configureWindow()
 		
@@ -30,14 +31,6 @@ class SceneDelegateTests: XCTestCase {
 		
 		XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
 		XCTAssertTrue(topController is ListViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
-	}
-	
-	private class UIWindowSpy: UIWindow {
-		var makeKeyAndVisibleCallCount = 0
-		
-		override func makeKeyAndVisible() {
-			makeKeyAndVisibleCallCount = 1
-		}
 	}
 	
 }
